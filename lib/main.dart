c9ecc103abc2b496649f63b1a8c4a64f7cedb56f
Đặng Hoàng Gia Phú <zafus2103@gmail.com>
2023-07-21 22:16:29 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:mock_prj1/screens/DualFormScreen.dart';
 import 'package:mock_prj1/screens/EditProfile.dart';
 
-
 void main() {
-  runApp( MaterialApp(home: DualFormScreen()));
+  runApp(MaterialApp(home: DualFormScreen()));
 }
