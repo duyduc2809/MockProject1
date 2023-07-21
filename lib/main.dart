0100325a7ef92b061f7521f4bd390739695ba0a7
Duy Duc Nguyen <duyduck2809@gmail.com>
2023-07-21 19:38:12 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mock_prj1/screens/EditProfile.dart';
 import 'package:mock_prj1/screens/SignInScreen.dart';
 
 void main() {
-  runApp(const SignInScreen());
+  runApp(const EditProfile());
 }
