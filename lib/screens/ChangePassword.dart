9c8e7efa75dc47268cd8ad659ffe2cb0eece959a
Doviet992001 <dohoangviet992001@gmail.com>
2023-07-22 00:14:35 +0700

    Update ChangePassword.dart

--- lib/screens/ChangePassword.dart
+++ lib/screens/ChangePassword.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mock_prj1/Validator.dart';
 import 'package:mock_prj1/constants/TextStyleConstant.dart';
+import 'package:mock_prj1/screens/homeScreen.dart';
 import '../classes/Account.dart';
 
 class ChangePassWord extends StatefulWidget {
@@ -18,68 +19,71 @@ class _ChangePassWordState extends State<ChangePassWord> {
 
   @override
   Widget build(BuildContext context) {
-    return 
-       Scaffold(
-          appBar: AppBar(
-            title: const Text('Change Password'),
-          ),
-          body: Container(
-            padding: const EdgeInsets.all(10),
-            child: Form(
-                key: _formkey,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(height: 10),
-                    const Text('Change Your Password'),
-                    const SizedBox(height: 20),
-                    TextFormField(
-                      controller: _currentPassController,
-                      decoration: const InputDecoration(
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Change Password'),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(10),
+          child: Form(
+              key: _formkey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 10),
+                  const Text('Change Your Password'),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: _currentPassController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter current password',
+                      hintStyle: hintTextStyle,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    controller: _newPassController,
+                    validator: (value) => Validator.passwordValidator(value),
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter new password',
+                      hintStyle: hintTextStyle,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    controller: _confirmnewPassController,
+                    validator: (value) => Validator.confirmPasswordValidator(
+                        value, _newPassController),
+                    decoration: const InputDecoration(
                         border: OutlineInputBorder(),
-                        hintText: 'Enter current password',
-                        hintStyle: hintTextStyle,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    TextFormField(
-                      controller: _newPassController,
-                      validator: (value) => Validator.passwordValidator(value),
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Enter new password',
-                        hintStyle: hintTextStyle,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    TextFormField(
-                      controller: _confirmnewPassController,
-                      validator: (value) => Validator.confirmPasswordValidator(
-                          value, _newPassController),
-                      decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'Enter password again',
-                          hintStyle: hintTextStyle),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        ElevatedButton(
-                            onPressed: () {}, child: const Text('Change')),
-                        ElevatedButton(
-                            onPressed: () {}, child: const Text('Home'))
-                      ],
-                    ),
-                  ],
-                )),
-          ));
+                        hintText: 'Enter password again',
+                        hintStyle: hintTextStyle),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ElevatedButton(
+                          onPressed: () {}, child: const Text('Change')),
+                      ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => const HomePage()));
+                          },
+                          child: const Text('Home'))
+                    ],
+                  ),
+                ],
+              )),
+        ));
   }
 }
