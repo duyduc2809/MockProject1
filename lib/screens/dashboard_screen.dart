738428c095d1bf73e312ba4782c8e945ed91891f
Doviet992001 <dohoangviet992001@gmail.com>
2023-07-26 18:49:41 +0700

    Update dashboard_screen.dart

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -2,14 +2,20 @@ import 'package:pie_chart/pie_chart.dart';
 import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
-class DashboardForm extends StatelessWidget {
+class DashboardForm extends StatefulWidget {
+
+  DashboardForm({super.key});
+
+  @override
+  State<DashboardForm> createState() => _DashboardFormState();
+}
+
+class _DashboardFormState extends State<DashboardForm> {
   Map<String, double> dataMap = {
     "Pending": 7,
     "Done": 2.4,
     "Processing": 2.4,
   };
-
-  DashboardForm({super.key});
 
   @override
   Widget build(BuildContext context) {
