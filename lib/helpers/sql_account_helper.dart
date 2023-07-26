bd73c96d389cfd79571a21debd062dee6570e26c
Doviet992001 <dohoangviet992001@gmail.com>
2023-07-26 14:35:47 +0700

    update

--- lib/helpers/sql_account_helper.dart
+++ lib/helpers/sql_account_helper.dart
@@ -133,43 +133,4 @@ class SQLAccountHelper {
     }
   }
 
-  // static Future<int> createItem(Item item) async {
-  //   final db = await SQLAccountHelper.db();
-  //   final id = await db.insert('items', item.toMap(),
-  //       conflictAlgorithm: ConflictAlgorithm.replace);
-
-  //   return id;
-  // }
-
-  // static Future<List<Map<String, dynamic>>> getItems(String function) async {
-  //   final db = await SQLAccountHelper.db();
-
-  //   return db.query('items',
-  //       orderBy: "id", where: "function = ?", whereArgs: [function]);
-  // }
-
-  // static Future<List<Map<String, dynamic>>> getItem(int id) async {
-  //   final db = await SQLAccountHelper.db();
-
-  //   return db.query('items', where: "id = ?", whereArgs: [id], limit: 1);
-  // }
-
-  // static Future<int> updateItem(Item item) async {
-  //   final db = await SQLAccountHelper.db();
-
-  //   final result = await db
-  //       .update('items', item.toMap(), where: "id = ?", whereArgs: [item.id]);
-
-  //   return result;
-  // }
-
-  // static Future<void> deleteItem(int id) async {
-  //   final db = await SQLAccountHelper.db();
-
-  //   try {
-  //     await db.delete("items", where: "id = ?", whereArgs: [id]);
-  //   } catch (err) {
-  //     debugPrint("ST wrong : $err");
-  //   }
-  // }
 }
