750d899f77e6a22bda1f8c38ecaae5cab5cc09ad
Đặng Hoàng Gia Phú <zafus2103@gmail.com>
2023-07-26 16:28:32 +0700

    commit
    display account current information, change button

--- lib/screens/change_password_screen.dart
+++ lib/screens/change_password_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mock_prj1/validator.dart';
 import 'package:mock_prj1/screens/home_screen.dart';
+import '../constants/dimension_constant.dart';
 import '../widgets/custom_input_decoration.dart';
 import '../helpers/sql_account_helper.dart';
 import '../classes/account.dart';
@@ -35,124 +36,127 @@ class _ChangePassWordState extends State<ChangePassWord> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SingleChildScrollView(
-          padding: const EdgeInsets.all(20),
-          child: Form(
-              key: _formkey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
+      padding: const EdgeInsets.all(20),
+      child: Form(
+          key: _formkey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 10),
+              const Row(
                 children: [
-                  const SizedBox(height: 10),
-                  const Row(
-                    children: [
-                      Icon(
-                        Icons.lock_open,
-                        size: 40,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        'Change your password',
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      )
-                    ],
+                  Icon(
+                    Icons.lock_open,
+                    size: 40,
                   ),
-                  const SizedBox(height: 20),
-                  TextFormField(
-                    obscureText: _isCurrentPassObscure,
-                    controller: _currentPassController,
-                    validator: (value) => Validator.passwordValidator(value),
-                    decoration: CustomInputDecoration(
-                      labelText: 'Current password',
-                      suffixIcon: IconButton(
-                        icon: _isCurrentPassObscure
-                            ? const Icon(Icons.visibility_off)
-                            : const Icon(Icons.visibility),
-                        onPressed: () {
-                          setState(() {
-                            _isCurrentPassObscure = !_isCurrentPassObscure;
-                          });
-                        },
-                      ),
-                    ),
+                  SizedBox(
+                    width: 10,
                   ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    obscureText: _isNewPassObscure,
-                    controller: _newPassController,
-                    validator: (value) => Validator.confirmPasswordValidator(
-                        value, _newPassController),
-                    decoration: CustomInputDecoration(
-                      labelText: 'New password',
-                      suffixIcon: IconButton(
-                        icon: _isNewPassObscure
-                            ? const Icon(Icons.visibility_off)
-                            : const Icon(Icons.visibility),
-                        onPressed: () {
-                          setState(() {
-                            _isNewPassObscure = !_isNewPassObscure;
-                          });
-                        },
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    obscureText: _isConfirmPassObscure,
-                    controller: _confirmnewPassController,
-                    validator: (value) => Validator.passwordValidator(value),
-                    decoration: CustomInputDecoration(
-                      labelText: 'Confirm new password',
-                      suffixIcon: IconButton(
-                        icon: _isConfirmPassObscure
-                            ? const Icon(Icons.visibility_off)
-                            : const Icon(Icons.visibility),
-                        onPressed: () {
-                          setState(() {
-                            _isConfirmPassObscure = !_isConfirmPassObscure;
-                          });
-                        },
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      ElevatedButton(
-                          onPressed: () {
-                            if (_formkey.currentState!.validate()) {
-                              SQLAccountHelper.updateAccount(Account(
-                                  id: SQLAccountHelper.currentAccount['id'],
-                                  email:
-                                      SQLAccountHelper.currentAccount['email'],
-                                  password: _newPassController.text));
-
-                              print(SQLAccountHelper.currentAccount['id']);
-                            }
-                            ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                    content: Text('Change successful! ')));
-                          },
-                          child: const Text('Change')),
-                      ElevatedButton(
-                          onPressed: () {
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => const HomePage()));
-                          },
-                          child: const Text('Home'))
-                    ],
-                  ),
+                  Text(
+                    'Change your password',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  )
                 ],
-              )),
-        ));
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                obscureText: _isCurrentPassObscure,
+                controller: _currentPassController,
+                validator: (value) => Validator.passwordValidator(value),
+                decoration: CustomInputDecoration(
+                  labelText: 'Current password',
+                  suffixIcon: IconButton(
+                    icon: _isCurrentPassObscure
+                        ? const Icon(Icons.visibility_off)
+                        : const Icon(Icons.visibility),
+                    onPressed: () {
+                      setState(() {
+                        _isCurrentPassObscure = !_isCurrentPassObscure;
+                      });
+                    },
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                obscureText: _isNewPassObscure,
+                controller: _newPassController,
+                validator: (value) => Validator.confirmPasswordValidator(
+                    value, _newPassController),
+                decoration: CustomInputDecoration(
+                  labelText: 'New password',
+                  suffixIcon: IconButton(
+                    icon: _isNewPassObscure
+                        ? const Icon(Icons.visibility_off)
+                        : const Icon(Icons.visibility),
+                    onPressed: () {
+                      setState(() {
+                        _isNewPassObscure = !_isNewPassObscure;
+                      });
+                    },
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                obscureText: _isConfirmPassObscure,
+                controller: _confirmnewPassController,
+                validator: (value) => Validator.passwordValidator(value),
+                decoration: CustomInputDecoration(
+                  labelText: 'Confirm new password',
+                  suffixIcon: IconButton(
+                    icon: _isConfirmPassObscure
+                        ? const Icon(Icons.visibility_off)
+                        : const Icon(Icons.visibility),
+                    onPressed: () {
+                      setState(() {
+                        _isConfirmPassObscure = !_isConfirmPassObscure;
+                      });
+                    },
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: const Size.fromHeight(55),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(kMediumPadding))),
+                  onPressed: () {
+                    if (_formkey.currentState!.validate()) {
+                      SQLAccountHelper.updateAccount(Account(
+                          id: SQLAccountHelper.currentAccount['id'],
+                          email: SQLAccountHelper.currentAccount['email'],
+                          password: _newPassController.text));
+
+                      print(SQLAccountHelper.currentAccount['id']);
+                    }
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Change successful! ')));
+                  },
+                  child: const Text('Change')),
+              const SizedBox(
+                height: spaceBetweenField / 2,
+              ),
+              OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                      minimumSize: const Size.fromHeight(55),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(kMediumPadding))),
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => const HomePage()));
+                  },
+                  child: const Text('Home')),
+            ],
+          )),
+    ));
   }
 }
