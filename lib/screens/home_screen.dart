ac6f12b95d5082004fe5c263954e3cb9b5b6ee39
Doviet992001 <dohoangviet992001@gmail.com>
2023-07-26 11:22:00 +0700

    update home

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,14 +1,15 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:mock_prj1/screens/category_screen.dart';
-import 'package:mock_prj1/screens/change_password_screen.dart';
-import 'package:mock_prj1/screens/dashboard_screen.dart';
-import 'package:mock_prj1/screens/edit_profile_screen.dart';
-import 'package:mock_prj1/screens/note_screen.dart';
 import 'package:mock_prj1/screens/priority_screen.dart';
 import 'package:mock_prj1/screens/status_screen.dart';
 import '../helpers/pref_helper.dart';
 import '../helpers/sql_account_helper.dart';
 import 'authentication_screen.dart';
+import 'change_password_screen.dart';
+import 'dashboard_screen.dart';
+import 'edit_profile_screen.dart';
+import 'note_screen.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,166 +19,152 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  late int _currentBody;
-  static const _dashboardText = 'Dashboard';
-  static const _categoryText = 'Category';
-  static const _changePasswordText = 'Change Password';
-  static const _editProfileText = 'Edit Profile';
-  static const _priorityText = 'Priority';
-  static const _statusText = 'Status';
-  static const _noteText = 'Note';
+  List pages = [DashboardForm(), const EditProfile(), const ChangePassWord()];
+  int currentIndex = 0;
+
+  void onTap(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    _currentBody = 0;
-  }
-
-  Widget _buildBody() {
-    switch (_currentBody) {
-      case 0:
-        return DashboardForm();
-      case 1:
-        return const CategoryItemScreen();
-      case 2:
-        return const PriorityItemScreen();
-      case 3:
-        return const StatusItemScreen();
-      case 4:
-        return AddNoteScreen();
-      case 5:
-        return const ChangePassWord();
-      case 6:
-        return const EditProfile();
-      default:
-        return const HomePage();
-    }
-  }
-
-  String _buildAppBarTitle() {
-    switch (_currentBody) {
-      case 0:
-        return _dashboardText;
-      case 1:
-        return _categoryText;
-      case 2:
-        return _priorityText;
-      case 3:
-        return _statusText;
-      case 4:
-        return _noteText;
-      case 5:
-        return _changePasswordText;
-      case 6:
-        return _editProfileText;
-      default:
-        return '';
-    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _buildBody(),
       appBar: AppBar(
-        title: Text(_buildAppBarTitle()),
+        title: const Text('Home Page'),
       ),
-      drawer: Drawer(
-        child: Padding(
-          padding: const EdgeInsets.all(30),
+      drawer: const NavigationDrawer(),
+    );
+  }
+}
+
+class NavigationDrawer extends StatelessWidget {
+  const NavigationDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) => Drawer(
+        child: SingleChildScrollView(
           child: Column(
-            children: [
-              Image.asset('assets/images/logo.jpg'),
-              Text('${SQLAccountHelper.currentAccount['email']}'),
-              ListTile(
-                title: const Text(
-                  _dashboardText,
-                ),
-                leading: const Icon(Icons.dashboard),
-                onTap: () {
-                  Navigator.pop(context);
-                  setState(() {
-                    _currentBody = 0;
-                  });
-                },
-              ),
-              ListTile(
-                title: const Text(_categoryText),
-                leading: const Icon(Icons.category),
-                onTap: () {
-                  Navigator.pop(context);
-                  setState(() {
-                    _currentBody = 1;
-                  });
-                },
-              ),
-              ListTile(
-                title: const Text(_priorityText),
-                leading: const Icon(Icons.low_priority_sharp),
-                onTap: () {
-                  Navigator.pop(context);
-                  setState(() {
-                    _currentBody = 2;
-                  });
-                },
-              ),
-              ListTile(
-                title: const Text(_statusText),
-                leading: const Icon(Icons.signal_wifi_statusbar_null),
-                onTap: () {
-                  Navigator.pop(context);
-                  setState(() {
-                    _currentBody = 3;
-                  });
-                },
-              ),
-              ListTile(
-                title: const Text(_noteText),
-                leading: const Icon(Icons.note_add),
-                onTap: () {
-                  Navigator.pop(context);
-                  setState(() {
-                    _currentBody = 4;
-                  });
-                },
-              ),
-              const Text("Account"),
-              ListTile(
-                title: const Text(_changePasswordText),
-                leading: const Icon(Icons.change_circle),
-                onTap: () {
-                  Navigator.pop(context);
-                  setState(() {
-                    _currentBody = 5;
-                  });
-                },
-              ),
-              ListTile(
-                title: const Text(_editProfileText),
-                leading: const Icon(Icons.edit),
-                onTap: () {
-                  Navigator.pop(context);
-                  setState(() {
-                    _currentBody = 6;
-                  });
-                },
-              ),
-              ListTile(
-                title: const Text("Log out"),
-                leading: const Icon(Icons.logout),
-                onTap: () {
-                  Navigator.of(context).pushAndRemoveUntil(
-                      MaterialPageRoute(builder: (context) {
-                    PrefHelper.clearSavedCredentials();
-                    return const DualFormScreen();
-                  }), (route) => false);
-                },
-              )
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildHeader(context),
+              buildMenuItems(context),
             ],
           ),
         ),
-      ),
-    );
+      );
+
+  Widget buildHeader(BuildContext context) => Container(
+        padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+      );
+
+  Widget buildMenuItems(BuildContext context) => Column(
+        children: [
+          DrawerHeader(child: Image.asset("assets/images/logo.jpg")),
+          Text('${SQLAccountHelper.currentAccount['email']}'),
+          DrawerListTile(
+            title: "Dashboard",
+            icon: Icons.dashboard_sharp,
+            press: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => DashboardForm()));
+            },
+          ),
+          DrawerListTile(
+            title: "Category",
+            icon: Icons.category,
+            press: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => const CategoryItemScreen(),
+              ));
+            },
+          ),
+          DrawerListTile(
+            title: "Priority",
+            icon: Icons.low_priority_sharp,
+            press: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => const PriorityItemScreen(),
+              ));
+            },
+          ),
+          DrawerListTile(
+            title: "Status",
+            icon: Icons.signal_wifi_statusbar_null,
+            press: () {
+
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => const StatusItemScreen(),
+              ));
+            },
+          ),
+          DrawerListTile(
+            title: "Note",
+            icon: Icons.note_add,
+            press: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => AddNoteScreen()));
+            },
+          ),
+          const Text(
+            'Account',
+            style: TextStyle(fontSize: 18, color: Colors.black),
+          ),
+          DrawerListTile(
+            title: "Edit profile",
+            icon: Icons.edit,
+            press: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => const EditProfile()));
+            },
+          ),
+          DrawerListTile(
+            title: "Change password",
+            icon: Icons.change_circle,
+            press: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => const ChangePassWord()));
+            },
+          ),
+          DrawerListTile(
+            title: "Log out",
+            icon: Icons.logout,
+            press: () {
+              Navigator.of(context).pushAndRemoveUntil(
+                  MaterialPageRoute(builder: (context) {
+                PrefHelper.clearSavedCredentials();
+                return const DualFormScreen();
+              }), (route) => false);
+            },
+          ),
+        ],
+      );
+}
+
+class DrawerListTile extends StatelessWidget {
+  const DrawerListTile({
+    Key? key,
+    required this.title,
+    required this.press,
+    required this.icon,
+  }) : super(key: key);
+
+  final String title;
+  final IconData icon;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+        horizontalTitleGap: 0.0,
+        leading: Icon(icon),
+        title: Text(title),
+        onTap: press);
   }
 }
